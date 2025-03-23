0bf88454b86b53344e00f602811bdc760f6c5966
Wilson-Pedro <wilsonpedroneto@gmail.com>
2025-03-23 20:20:43 -0300

    feat: registering anamnesis

--- pages/anamnese.php
+++ pages/anamnese.php
@@ -1,3 +1,44 @@
+<?php
+    include('../db/conexao.php');
+
+    try {
+
+        if(isset($_POST['finalizar'])) {
+            $motivo_consulta = $_POST['motivo_consulta'];
+            $inicio_sintomas = $_POST['inicio_sintomas'];
+            $descricao_sintomas = $_POST['descricao_sintomas'];
+            $aconteceu_antes = $_POST['aconteceu_antes'];
+            $tem_doencas_cronicas = $_POST['doenca_cronica'];
+            $doencas_cronicas_descricao = !empty($_POST['doencas_cronicas_descricao']) ? $_POST['doencas_cronicas_descricao']  : 'sem doenças crônicas';
+            $tem_alergias = $_POST['alergias'];
+            $alergias_descricao = !empty($_POST['alergias_descricao']) ? $_POST['alergias_descricao']  : 'sem alergias';
+            $medicamentos = $_POST['medicamentos'];
+            $medicamentos_descricao = !empty($_POST['medicamentos_descricao']) ? $_POST['medicamentos_descricao']  : 'não toma medicamentos contínuos';
+            $doencas_familiares = $_POST['doencas_familiares'];
+            $doencas_familiares_descricao = !empty($_POST['doencas_familiares_descricao']) ? $_POST['doencas_familiares_descricao']  : 'Não tem doenças familiares';
+            $fuma = $_POST['fuma'];
+            $alcool = $_POST['alcool'];
+            $atividade_fisica = $_POST['atividade_fisica'];
+    
+            $stmt_anamnese = $mysqli->prepare("INSERT INTO tbl_anamnese 
+            (id, motivo, inicio_sintoma, descricao_sintoma, ja_aconteceu_antes, tem_doencas_cronicas, doencas_cronicas, tem_alergias, alergias, usa_medicamentos_continuos, medicamentos_continuos, tem_doencas_familia, doencas_familia, fuma, ingere_alcool, atividade_fisica) 
+            VALUES (NULL,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+            $stmt_anamnese->bind_param("sssiisisisisiii", 
+            $motivo_consulta, $inicio_sintomas, $descricao_sintomas, $aconteceu_antes, $tem_doencas_cronicas, $doencas_cronicas_descricao, 
+            $tem_alergias, $alergias_descricao, $medicamentos, $medicamentos_descricao, $doencas_familiares, $doencas_familiares_descricao, 
+            $fuma, $alcool, $atividade_fisica);
+            $stmt_anamnese->execute();
+            $stmt_anamnese->close();
+
+            header('Location: cadastro-sucesso.php');
+        }
+
+    } catch(Exception $e) {
+        echo "Error: " . $e;
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,31 +56,31 @@
         <a href="Home.php"><i class="bi bi-arrow-left-circle-fill" id="iconeVoltar"></i></a>
     </div>
 
-    <div class="container">
+    <form method="post" class="container">
         <h2>Anamnese</h2>
 
         <div class="form-group">
             <label for="motivo-consulta">Motivo da consulta</label>
-            <input type="text" id="motivo-consulta" placeholder="Motivo da consulta">
+            <input type="text" id="motivo-consulta" name="motivo_consulta" placeholder="Motivo da consulta" required>
         </div>
 
         <div class="form-group">
             <label for="inicio-sintomas">Início dos sintomas</label>
-            <input type="text" id="inicio-sintomas" placeholder="Dia / Mês / Ano">
+            <input type="date" id="inicio-sintomas" name="inicio_sintomas" placeholder="Dia / Mês / Ano" required>
         </div>
 
         <div class="form-group">
             <label for="descricao-sintomas">Descrição dos sintomas</label>
-            <textarea id="descricao-sintomas" rows="4" placeholder="Descreva os sintomas..."></textarea>
+            <textarea id="descricao-sintomas" name="descricao_sintomas" rows="4" placeholder="Descreva os sintomas..." required></textarea>
         </div>
 
         <div class="form-group">
             <label>Já aconteceu antes?</label>
             <div class="checkbox-group">
-                <input type="radio" id="aconteceu-sim" name="aconteceu">
+                <input type="radio" id="aconteceu-sim" name="aconteceu_antes" value="1" required>
                 <label for="aconteceu-sim">Sim</label>
 
-                <input type="radio" id="aconteceu-nao" name="aconteceu" style="margin-left: 15px;">
+                <input type="radio" id="aconteceu-nao" name="aconteceu_antes" value="0" style="margin-left: 15px;" required>
                 <label for="aconteceu-nao">Não</label>
             </div>
         </div>
@@ -47,58 +88,58 @@
         <div class="form-group">
             <label for="doencas-cronicas">Doenças crônicas?</label>
             <div class="checkbox-group">
-                <input type="checkbox" id="doenca-cronica-sim">
+                <input type="radio" id="doenca-cronica-sim" name="doenca_cronica" value="1" required>
                 <label for="doenca-cronica-sim">Sim</label>
 
-                <input type="checkbox" id="doenca-cronica-nao" style="margin-left: 15px;">
+                <input type="radio" id="doenca-cronica-nao" name="doenca_cronica" value="0" style="margin-left: 15px;" required>
                 <label for="doenca-cronica-nao">Não</label>
             </div>
-            <input type="text" id="quais-doencas" placeholder="Se sim, quais?" style="margin-top: 10px;">
+            <input type="text" id="quais-doencas" name="doencas_cronicas_descricao" placeholder="Se sim, quais?" style="margin-top: 10px;">
         </div>
 
         <div class="form-group">
             <label for="alergias">Alergias?</label>
             <div class="checkbox-group">
-                <input type="checkbox" id="alergias-sim">
+                <input type="radio" id="alergias-sim" name="alergias" value="1" required>
                 <label for="alergias-sim">Sim</label>
 
-                <input type="checkbox" id="alergias-nao" style="margin-left: 15px;">
+                <input type="radio" id="alergias-nao" name="alergias" value="0" style="margin-left: 15px;" required>
                 <label for="alergias-nao">Não</label>
             </div>
-            <input type="text" id="quais-alergias" placeholder="Se sim, quais?" style="margin-top: 10px;">
+            <input type="text" id="quais-alergias"name="alergias_descricao" placeholder="Se sim, quais?" style="margin-top: 10px;">
         </div>
 
         <div class="form-group">
             <label for="medicamentos">Medicamentos contínuos?</label>
             <div class="checkbox-group">
-                <input type="checkbox" id="medicamentos-sim">
+                <input type="radio" id="medicamentos-sim" name="medicamentos" value="1" required>
                 <label for="medicamentos-sim">Sim</label>
 
-                <input type="checkbox" id="medicamentos-nao" style="margin-left: 15px;">
+                <input type="radio" id="medicamentos-nao" name="medicamentos" value="0" style="margin-left: 15px;" required>
                 <label for="medicamentos-nao">Não</label>
             </div>
-            <input type="text" id="quais-medicamentos" placeholder="Se sim, quais?" style="margin-top: 10px;">
+            <input type="text" id="quais-medicamentos" name="medicamentos_descricao" placeholder="Se sim, quais?" style="margin-top: 10px;">
         </div>
 
         <div class="form-group">
             <label for="doencas-familia">Doenças na família?</label>
             <div class="checkbox-group">
-                <input type="checkbox" id="doenca-familia-sim">
+                <input type="radio" id="doenca-familia-sim" name="doencas_familiares" value="1" required>
                 <label for="doenca-familia-sim">Sim</label>
 
-                <input type="checkbox" id="doenca-familia-nao" style="margin-left: 15px;">
+                <input type="radio" id="doenca-familia-nao" name="doencas_familiares" value="0" style="margin-left: 15px;" required>
                 <label for="doenca-familia-nao">Não</label>
             </div>
-            <input type="text" id="quais-doencas-familia" placeholder="Se sim, quais?" style="margin-top: 10px;">
+            <input type="text" id="quais-doencas-familia" name="doencas_familiares_descricao" placeholder="Se sim, quais?" style="margin-top: 10px;">
         </div>
 
         <div class="form-group">
             <label>Fuma?</label>
             <div class="checkbox-group">
-                <input type="radio" id="fuma-sim" name="fuma">
+                <input type="radio" id="fuma-sim" name="fuma" value="1" required>
                 <label for="fuma-sim">Sim</label>
 
-                <input type="radio" id="fuma-nao" name="fuma" style="margin-left: 15px;">
+                <input type="radio" id="fuma-nao" name="fuma" value="0" style="margin-left: 15px;" required>
                 <label for="fuma-nao">Não</label>
             </div>
         </div>
@@ -106,10 +147,10 @@
         <div class="form-group">
             <label>Ingere álcool?</label>
             <div class="checkbox-group">
-                <input type="radio" id="alcohol-sim" name="alcohol">
+                <input type="radio" id="alcohol-sim" name="alcool" value="1" required>
                 <label for="alcohol-sim">Sim</label>
 
-                <input type="radio" id="alcohol-nao" name="alcohol" style="margin-left: 15px;">
+                <input type="radio" id="alcohol-nao" name="alcool" value="0" style="margin-left: 15px;" required>
                 <label for="alcohol-nao">Não</label>
             </div>
         </div>
@@ -117,19 +158,19 @@
         <div class="form-group">
             <label>Pratica atividade física?</label>
             <div class="checkbox-group">
-                <input type="radio" id="atividade-fisica-sim" name="atividade-fisica">
+                <input type="radio" id="atividade-fisica-sim" name="atividade_fisica" value="1" required>
                 <label for="atividade-fisica-sim">Sim</label>
 
-                <input type="radio" id="atividade-fisica-nao" name="atividade-fisica" style="margin-left: 15px;">
+                <input type="radio" id="atividade-fisica-nao" name="atividade_fisica" value="0" style="margin-left: 15px;" required>
                 <label for="atividade-fisica-nao">Não</label>
             </div>
         </div>
 
         <div class="submit-btn">
-            <button type="submit">Próximo</button>
+            <button type="submit" name="finalizar">Próximo</button>
         </div>
 
-    </div>
+    </form>
 
 </body>
 </html>
